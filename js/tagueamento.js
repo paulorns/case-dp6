b8616e608c13e0e4c1e9ec80a5aebcbae06f2558
paulorns <htx.prs@gmail.com>
2021-08-25 20:23:55 -0300

    adicionando os arquivos do case e com as funcoes no tagueamento.js funcionando

---
+++ js/tagueamento.js
@@ -0,0 +1,36 @@
+// Preencha este arquivo com qualquer código que você necessite para realizar a
+// coleta, desde a biblioteca analytics.js, gtag.js ou o snippet do Google Tag 
+// Manager. No último caso, não é necessário implementar a tag <noscript>.
+// O ambiente dispõe da jQuery 3.5.1, então caso deseje, poderá utilizá-la
+// para fazer a sua coleta.
+// Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
+
+//entre-contato
+function notifContato(){
+  console.log("entre-contato");
+}
+
+//download-pdf
+function notifDownload(){
+  console.log("download-pdf");
+}
+
+//bt-lorem
+function notifLorem(){
+	console.log("bt-lorem");
+}
+
+//bt-ipsum
+function notifIpsum(){
+	console.log("bt-ipsum");
+}
+
+//bt-dolor
+function notifDolor(){
+	console.log("bt-dolor");
+}
+
+//enviar
+function notifEnviar(){
+	console.log("enviar");
+}
